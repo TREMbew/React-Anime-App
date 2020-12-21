d0cdeb49fb066590a1d434c041d1c38d64e41d76
Mohamed Mhamdi <mhamdi.mohamed944@gmail.com>
2020-12-21 17:37:52 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import {Data} from './data';
 import {MovieList} from './Component/MovieList';
+import {FilterV2} from './Component/FilterV2';
 import {useState} from 'react';
 
 function App() {
-  const [anime, setAnime] = useState(Data)
+  const [anime, setAnime] = useState(Data);
+  const [rateValue, setRateValue] = useState(5);
+  const [inputFilter, setInputFilter] = useState('');
+  const filterHandler = (searchTerm) => {
+    setInputFilter(searchTerm);
+  }
   return (
     <div className="App">
-      <MovieList animelist={anime} setanime={setAnime} />
+      <FilterV2 ratevalue={rateValue} setratevalue={setRateValue} searching={filterHandler}/>
+      <MovieList animelist={anime.filter(el => el.rate <= rateValue && el.title.toLocaleLowerCase().includes(inputFilter.toLocaleLowerCase()))} setanime={setAnime} />
     </div>
   );
 }
--- src/Component/MovieList.js
+++ src/Component/MovieList.js
@@ -1,21 +1,12 @@
 import {MovieCard} from './MovieCard';
 import {AddMovie} from './AddMovie';
-import {Filter} from './Filter';
-//import React, {useState, useEffect} from 'react';
 
 export const MovieList = ({animelist, setanime}) => {
     const handleOk = (addTerm) => {
         setanime([...animelist, addTerm]);
     };
-    const ratingSetValueHandler = (rating) => {
-        setanime(animelist.filter(el => el.rate <= rating ));
-    };
-    const searchSubmitHandler = (searchTerm) => {
-        setanime(animelist.filter(el => el.title.toLowerCase().indexOf(searchTerm) > -1))        
-    }
     return (
         <div>
-            <Filter onChange={ratingSetValueHandler} onSubmit={searchSubmitHandler}/>
             <AddMovie onOk={handleOk}/>
             <div className='anime-container'>
                 {animelist.map((el,i) => (

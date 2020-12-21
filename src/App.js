fa26644e150fd6053692990ac0042edb844e9c92
Mohamed Mhamdi <mhamdi.mohamed944@gmail.com>
2020-12-21 12:45:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Data} from './data';
+import {MovieList} from './Component/MovieList';
+import {useState} from 'react';
 
 function App() {
+  const [anime, setAnime] = useState(Data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieList animelist={anime} setanime={setAnime} />
     </div>
   );
 }
